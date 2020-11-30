3292e3b231c45cefaea55b15375e7312fedb69cf
Rajat Goyal <rajatgoyalofficial007@gmail.com>
2020-11-30 13:07:51 +0530

    Modified Dice simulator code with loop.

--- dice_simulator.py
+++ dice_simulator.py
@@ -1,4 +1,12 @@
 import random
 
-s = random.randrange(1,7)
-print(s)
+
+while True:
+    print(f"Player 1st Number = : ", random.randrange(1, 7))
+    d = input("\n\nClick to get number for PLAYER 2.\n& Press 'q' to exit the game.\n")
+    if d == 'q':
+        break
+    print(f"Player 2nd Number = : ", random.randrange(1, 7))
+    s = input("\nClick to get number for PLAYER 1.\n& Press 'q' to exit the game.\n")
+    if s == 'q':
+        break
